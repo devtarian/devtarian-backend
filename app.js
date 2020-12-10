9dc4654b33bf3f2d22e9ae092cbdf276821335cf
kyungyoonha <gkb10a@naver.com>
2020-12-10 13:21:59 +0900

    기능추가: Heroku DB연동

--- app.js
+++ app.js
@@ -4,13 +4,29 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 
+const db = require("./models");
+
 const app = express();
 const port = 8000;
 
+// [ DB ]
+db.sequelize
+    .authenticate()
+    .then(() => {
+        console.log("Connection has been established successfully.");
+        return db.sequelize.sync();
+        //return db.sequelize.drop();
+    })
+    .then(() => {
+        console.log("DB Sync complete.");
+    })
+    .catch((err) => {
+        console.error("Unable to connect to the database:", err);
+    });
+// [ Middleware ]
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-
 // [ Static ]
 app.use("/uploads", express.static(path.resolve(__dirname, "uploads")));
 // [ Routes ]
--- models/index.js
+++ models/index.js
@@ -1,5 +1,38 @@
-const Sequelize = require("sequelize");
-const path = reuqire("path");
-const fs = require("fs");
+let Sequelize = require("sequelize");
+let path = require("path");
+let fs = require("fs");
 
-const sequelize = new Sequelize();
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
+
+let db = {};
+fs.readdirSync(__dirname)
+    .filter((file) => {
+        return file.indexOf(".js") && file !== "index.js";
+    })
+    .forEach((file) => {
+        // let model = sequelize['import'](path.join(__dirname, file));
+        let model = require(path.join(__dirname, file))(
+            sequelize,
+            Sequelize.DataTypes
+        );
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach((modelName) => {
+    if ("associate" in db[modelName]) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
