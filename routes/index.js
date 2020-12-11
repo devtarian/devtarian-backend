956bf9e166f16f8b420aaf1c8440511e8c8e9df4
kyungyoonha <gkb10a@naver.com>
2020-12-11 14:34:21 +0900

    기능수정: Merge 에러 수정

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { User } = require("./models");
 
 router.use("/auth", require("./auth"));
-router.get("/", (req, res) => {
-    res.json({ message: "few" });
-});
 
 module.exports = router;
